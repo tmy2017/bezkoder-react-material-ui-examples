c12ab3abde165abd055bd1b62563b93a94302651
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2021-05-23 20:07:27 +0200

    switch to Vercel BE!

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://6jozz.sse.codesandbox.io/api",
+  //https://codesandbox.io/s/node-express-mongodb--bezkoder-6jozz?file=/package.json
+  // baseURL: "https://6jozz.sse.codesandbox.io/api",
+  //now vecel deploy working! zz-_20-_y21-0523-2007
+  baseURL: "https://csb-6nfgg-qugkak562-tklendaircraft.vercel.app/api/",
   headers: {
     "Content-type": "application/json"
   }
